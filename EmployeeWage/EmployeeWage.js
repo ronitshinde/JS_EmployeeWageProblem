4d976bfe681569b624cc1ad6938812b9e8b4931c
ronitshinde <rony.shinde753@gmail.com>
2023-04-07 23:42:11 +0530

    [Ronit] Add : UC1-Check Employee Is Present Or Absent

---
+++ EmployeeWage/EmployeeWage.js
@@ -0,0 +1,10 @@
+//UC1
+const isAbsent=0;
+let empCheck = Math.floor(Math.random()*10)%2;
+
+if(empCheck == isAbsent)
+console.log("The employee is absent")
+else
+console.log("The employee is present")
+
+
